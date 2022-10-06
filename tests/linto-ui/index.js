3eed72c7f72add46745f9e619a4eab25d8587349
rlopezdev <rlopez@linagora.com>
2022-10-06 11:58:41 +0200

    fix errors on askFeedback + add transactionMode option to select sendText mode (chatbot or command)

--- tests/linto-ui/index.js
+++ tests/linto-ui/index.js
@@ -1,15 +1,16 @@
-import LintoUI from '../../src/linto-ui.js'
+import LintoUI from "../../src/linto-ui.js"
 
 window.LintoUI = new LintoUI({
-    debug: true,
-    containerId: 'chatbot-wrapper',
-    lintoWebToken: 'v2lS299nR5Fv8k7Q',
-    lintoWebHost: 'https://stage.linto.ai/overwatch/local/web/login',
-    widgetMode: 'multi-modal'
+  debug: true,
+  containerId: "chatbot-wrapper",
+  lintoWebToken: "RDviNAcFYYbRUzAu",
+  lintoWebHost: "https://gamma.linto.ai/overwatch/local/web/login",
+  widgetMode: "minimal-streaming",
+  transactionMode: "command", // 'command' to use sendCommandText() ||'chatbot' to use sendChatbotText()
 })
 
-const formNameBtn = document.getElementById('form-name-button')
-formNameBtn.onclick = function() {
-    formNameBtn.classList.add('streaming-on')
-    window.LintoUI.customStreaming('vad-custom', 'form-name')
+const formNameBtn = document.getElementById("form-name-button")
+formNameBtn.onclick = function () {
+  formNameBtn.classList.add("streaming-on")
+  window.LintoUI.customStreaming("vad-custom", "form-name")
 }
